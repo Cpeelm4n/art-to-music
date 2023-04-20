208e68f15a89a8085672871afc64aabdaadaf6e2
Cpeelm4n <130704045+Cpeelm4n@users.noreply.github.com>
2023-04-20 16:12:54 -0400

    Update art-music_0.0.py
    Task layout

--- art-music_0.0.py
+++ art-music_0.0.py
@@ -2,5 +2,23 @@
 # CSC 1570 project to convert art to music
 # Caleb Peelman
 # 4/20/2023
-# Version: 0.0a
+# Version: 0.0b
 #-------------------------------------------]
+
+#Import libraries
+
+#Keep track of settings
+#Handle user input
+
+#See if they want to use their own image or a sample
+#See of we have an image
+
+
+
+#Save image into a temp file
+#Ask the computer to view the image
+#grab hue from HSV
+#separate the hues and convert to frequency
+#generic song save
+#make an effected version
+#save the final song and make a midi
